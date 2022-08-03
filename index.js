916147c0aee091e1f2daddeed9371711dc10e1a0
Pankaj <smartborse@gmail.com>
2022-08-03 20:44:54 +0530

    initial commit:

--- index.js
+++ index.js
@@ -1,30 +1,9 @@
 const hourE1 = document.getElementById("hour");
 
-const minuteE1 = document.getElementById("minute");
+const minuteE1 = document.getElementById("minutes");
 
-const secondE1 = documnet.getElementById("second");
+const secondE1 = documnet.getElementById("seconds");
 
 const ampmE1 = document.getElementById("ampm");
-
-function updateClock(){
-    let h = new Date().getHours();
-    let m = new Date().getMinutes();
-    let s = new Date().getSeconds();
-    let ampm = "AM";
-
-    if(h > 12 ){
-        h = h - 12;
-        ampm = "PM";
-    }
-
-    hourE1.innerText = h;
-    minuteE1.innerText = m;
-    secondE1.innerText = s;
-    ampmE1, (innerText = ampm);
-    setTimeout(() => {
-        updateClock();
-    }, 1000)
-
-}
 
 updateClock();
