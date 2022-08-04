9f38b11dfc2f0acc3c88535738d3388c20ac348e
pankaj <smartborse@gmail.com>
2022-08-04 09:06:50 +0530

    deleted some files

--- index.js
+++
@@ -1,9 +0,0 @@
-const hourE1 = document.getElementById("hour");
-
-const minuteE1 = document.getElementById("minutes");
-
-const secondE1 = documnet.getElementById("seconds");
-
-const ampmE1 = document.getElementById("ampm");
-
-updateClock();
